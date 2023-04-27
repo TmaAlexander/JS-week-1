3ffd1f4b5247aa0bb2df19f3a1b5889fc5015ee5
tmamailoalexander <t.mailoalexander21@gmail.com>
2023-04-28 09:44:02 +1200

    functions and prompts

---
+++ script.js
@@ -0,0 +1,80 @@
+// Generate a list using Javascript
+// document.getElementById('list-container').innerHTML = `
+// <ul class="music-list">
+//     <li>Lady Gaga</li>
+//     <li>Justin Beiber</li>
+//     <li>Rick Astley</li>
+// </ul>
+// `;
+
+// variables
+// set up a string variable with the data of Tim
+var userName = "Jake";
+// number variable example
+var age = 21;
+// boolean variables: true or false
+var hungry = false;
+
+// --view our data
+// console.log({ hungry });
+
+// --combining variables and strings
+// console.log(`Your age is ${age}`);
+// console.log(`Your name is ${userName}`);
+// console.log(`Your hunger status is: ${hungry}`);
+
+// --do an equation
+// console.log(`Your age times two is ${age * 2}`);
+// console.log(`Your age divided by 12 is ${age / 2}`);
+
+// --create an error
+// console.log(`Username multipled by two is ${userName * 2}`);
+// --showing multiple variables in the console log
+// console.log(`Your name is ${userName} and your age is ${age}`);
+
+// --alert the user in the browser
+// alert(`Hello ${userName}`);
+
+// --pushing variables into HTML elements
+// document.getElementById('greeting-h1').innerHTML = `Hello your username is ${userName}`;
+// document.getElementById('subheader').innerHTML = `Your hunger status is: ${hungry}`;
+
+// ---temperature converter
+// var tempCelcius = prompt("Enter a temperature in celcius?");
+
+// var farenheight = (tempCelcius * 9 / 5) + 32;
+
+// console.log({ farenheight });
+
+// ---Activity: show the full equation on the screen.
+// var resultDiv = document.getElementById("temp-result");
+
+// resultDiv.innerHTML = `
+// ${tempCelcius} deg celcius is equal to ${farenheight} deg farenheight.
+// `;
+
+// --functions
+
+// var firstName = prompt("What's your name?")
+
+// function helloUser() {
+//     alert(`Hello, ${firstName}`);
+// }
+// helloUser();
+
+// -----CLOUD
+var multiplyResult = document.getElementById("multiply-result");
+
+var num1 = prompt("first number");
+var num2 = prompt("second number");
+var num3 = prompt("third number");
+
+function multiply() {
+    var result = num1 * num2 * num3;
+
+}
+
+
+
+
+
